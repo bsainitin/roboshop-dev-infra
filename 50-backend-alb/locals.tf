b73673e60fc7fdfc964b3ed59de1adcd894ffcc1
bsainitin <b.sainitin@gmail.com>
2025-12-02 20:47:36 +0530

    Create backend-alb with listener

---
+++ 50-backend-alb/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_name_suffix = "${var.project_name}-${var.environment}"
+  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  common_tags = {
+    Project = var.project_name
+    Environmentv = var.environment
+    Terraform = "true"
+  }
+}
