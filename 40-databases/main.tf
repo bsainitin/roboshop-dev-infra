dbafa1040db54e5bf60effb1f70b62002bbdfaa7
bsainitin <b.sainitin@gmail.com>
2025-12-03 14:19:21 +0530

    Install Ansible in redis, Copy Ansible playbooks to redis server

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "mongodb" {
-    ami = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.mongodb_sg_id]
-    subnet_id = local.database_subnet_id
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mongodb_sg_id]
+  subnet_id = local.database_subnet_id
 
-    tags = merge(
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-mongodb"
-        }        
-    )
+  tags = merge(
+      local.common_tags,
+      {
+          Name = "${local.common_name_suffix}-mongodb"
+      }        
+  )
 }
 
 resource "terraform_data" "mongodb" {
@@ -35,6 +35,46 @@ resource "terraform_data" "mongodb" {
         "chmod +x /tmp/bootstrap.sh",
         # "sudo sh /tmp/bootstrap.sh"
         "sudo sh /tmp/bootstrap.sh mongodb"
+     ]
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_id
+
+  tags = merge(
+      local.common_tags,
+      {
+          Name = "${local.common_name_suffix}-redis"
+      }        
+  )
+}
+
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  # Terraform copies this file to remote server
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
      ]
   }
 }
