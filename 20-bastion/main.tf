6a0e9d3bb0b4813e5aa91815379a6dbf0560c190
bsainitin <b.sainitin@gmail.com>
2025-12-06 08:29:10 +0530

    Add Role to read AWS CLI Credentials

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,15 +1,22 @@
 resource "aws_instance" "bastion" {
-    ami = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.bastion_sg_id]
-    subnet_id = local.public_subnet_id
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id = local.public_subnet_id
 
-    user_data = file("bastion.sh")
+  user_data = file("bastion.sh")
 
-    tags = merge(
-        local.common_tags,
-        {
-            Name = "${var.project_name}-${var.environment}-bastion"
-        }        
-    )
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-bastion"
+    }        
+  )
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "AWSCLICredentialsRead"
 }
