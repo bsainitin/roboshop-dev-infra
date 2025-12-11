0931ebfdeee4722f8a54dab66221e3d4560c0da6
bsainitin <b.sainitin@gmail.com>
2025-12-11 12:42:59 +0530

    Resolved error in backend_alb allowing connections from frontend

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -209,7 +209,7 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
 resource "aws_security_group_rule" "backend_alb_frontend" {
   type   = "ingress"
   security_group_id = local.backend_alb_sg_id
-  source_security_group_id = local.frontend_alb_sg_id
+  source_security_group_id = local.frontend_sg_id
   from_port   = 80
   to_port     = 80
   protocol = "tcp"
